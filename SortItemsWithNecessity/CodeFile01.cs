33c85f85251f270c316046fdb2e9c047d5824241
wauchang <wu5324@gmail.com>
2021-01-16 21:52:39 +0900

    ver 0.1

--- SortItemsWithNecessity/CodeFile01.cs
+++ SortItemsWithNecessity/CodeFile01.cs
@@ -1,6 +1,7 @@
 ﻿using ClosedXML.Excel;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ClosedXMLTest
 {
@@ -34,7 +35,8 @@ namespace ClosedXMLTest
 
             //XLファイルのパスを入力
             Console.Write("Enter the Pass : ");
-            string pass = Console.ReadLine();
+            string pass = @"F:\wau\Documents\todoList.xlsx";
+            //string pass = Console.ReadLine();
 
             //ワークブック・シートを開く
             var workbook = new XLWorkbook(pass);
@@ -66,27 +68,71 @@ namespace ClosedXMLTest
             //比較するよ
             for (int index = 0; index < Items.Count; index++)
             {
-                Console.WriteLine("Compare " + Items[index].Name + " to others.");
-
-                obj.V1ComparedItems.Add(obj.V1Comparing(Items, index));
-
+                obj.V1ComparedItems.Add(obj.Comparing(Items, index, 1));
+                obj.V1ComparedItems.Sort((a, b) => (a.Value1 - b.Value1) * (-1));
+                obj.V2ComparedItems.Add(obj.Comparing(Items, index, 2));
+                obj.V2ComparedItems.Sort((a, b) => (a.Value2 - b.Value2) * (-1));
+                foreach (Item item in obj.V1ComparedItems)
+                {
+                    Console.WriteLine(item.Name);
+                }
             }
+
+            List<Item> ComparedItems = obj.V1ComparedItems;
+            ComparedItems.Sort((a, b) => (a.Number - b.Number));
+
+            //新規ブック・シート作成
+            var newBook = new XLWorkbook();
+            var newSheet = newBook.Worksheets.Add("New List");
+            newSheet.Cell(1, 1).Value = "No";
+            newSheet.Cell(1, 2).Value = "Name";
+            newSheet.Cell(1, 3).Value = "Necessity";
+            newSheet.Cell(1, 4).Value = "Interest";
+
+            for(int i = 0; i < ComparedItems.Count; i++)
+            {
+                newSheet.Cell(i + 2, 1).Value = ComparedItems[i].Number;
+                newSheet.Cell(i + 2, 2).Value = ComparedItems[i].Name;
+                newSheet.Cell(i + 2, 3).Value = ComparedItems[i].Value1;
+                newSheet.Cell(i + 2, 4).Value = ComparedItems[i].Value2;
+            }
+
+            //newBook.SaveAs(@"F:\wau\Documents\NewBook.xlsx");
+            newBook.SaveAs("NewBook.xlsx");
+
+
         }
 
-        //実際の比較はここで行う
-        Item V1Comparing(List<Item> Items, int index)
+        //ValueNumberに寄らない一般化をしたい
+        Item Comparing(List<Item> Items, int index, int valueNumber)
         {
-            bool matchSomeValue = false;　                                                  //比較完了を表すbool
-            List<Item> ComparedItems = V1ComparedItems;　　                //比較済みの要素
-            string question = " more necessary than ";                                   //v1,v2で聞くことの切り替え
+            bool endOfCompare = false;                  //比較完了を表すbool
+            List<Item> ComparedItems;                  //比較済みの要素
+            string question, result;                                      //v1,v2で聞くことの切り替え
+
+            if (valueNumber == 1)
+            {
+                ComparedItems = V1ComparedItems;
+                question = " more necessary than ";
+                result = "Necessity of ";
+            }
+            else
+            {
+                ComparedItems = V2ComparedItems;
+                question = " more interesting than ";
+                result = "Interest of ";
+            }
+
+
 
 
             //比較済みの各要素と比較
             foreach (Item item in ComparedItems)
             {
-                //比較する要素の値が比較先以下であり、かつ、どの要素の値ともイコールじゃない間ループする
-                //比較を進めて、全ての要素より大きくなるかどれかと価値が等しくなったらループを終了する
-                if (Items[index].Value1 <= item.Value1 && matchSomeValue == false)
+                //比較済みのリストは降順に並んでいるので、大きいものから順に比較を進める
+                //比較先より小さければ続行、大きいあるいは等しい場合は処理を終了する
+
+                if (endOfCompare == false)
                 {
                     char choice;
 
@@ -103,19 +149,37 @@ namespace ClosedXMLTest
                     {
                         case '0':　//比較先の要素と価値がイコールである場合、boolをtrueにして比較を終了する
                             Console.WriteLine(Items[index].Name + " == " + item.Name);
-                            Items[index].Value1 = item.Value1;
-                            matchSomeValue = true;
-                            break;
-                        case '1':　//比較先の要素より値が大きい場合、自信の値を比較先＋１して進める
-                            Console.WriteLine(Items[index].Name + " > " + item.Name);
-                            Items[index].Value1 = item.Value1 + 1;
-                            Console.WriteLine("Debug : comparingValue is " + Items[index].Value1);
-                            break;
-                        case '2':　//比較先の要素より値が小さい場合、比較済みの各要素の値を全て＋１して進める
-                            Console.WriteLine(Items[index].Name + " < " + item.Name);
-                            foreach (Item ComparedItem in ComparedItems)
+                            if (valueNumber == 1)
                             {
-                                if (ComparedItem.Value1 >= Items[index].Value1) ComparedItem.Value1++;
+                                Items[index].Value1 = item.Value1;
+                            }
+                            else
+                            {
+                                Items[index].Value2 = item.Value2;
+                            }
+                            endOfCompare = true;
+                            break;
+                        case '1':　//比較先の要素より値が大きい場合、自信の値を比較先＋１して比較を終了する
+                            Console.WriteLine(Items[index].Name + " > " + item.Name);
+                            if (valueNumber == 1)
+                            {
+                                Items[index].Value1 = item.Value1 + 1;
+                            }
+                            else
+                            {
+                                Items[index].Value2 = item.Value2 + 1;
+                            }
+                            endOfCompare = true;
+                            break;
+                        case '2':　//比較先の要素より値が小さい場合、比較済みの各要素の値を＋１して進める
+                            Console.WriteLine(Items[index].Name + " < " + item.Name);
+                            if (valueNumber == 1)
+                            {
+                                item.Value1++;
+                            }
+                            else
+                            {
+                                item.Value2++;
                             }
                             break;
                     }
@@ -123,93 +187,23 @@ namespace ClosedXMLTest
             }
 
             //比較が終わったら値を表示して、返す
-            foreach (Item ComparedItem in ComparedItems)
-            {
-                Console.WriteLine("Necessity of " + ComparedItem.Name + " : " + ComparedItem.Value1);
-            }
-            Console.WriteLine("Necessity of " + Items[index].Name + " : " + Items[index].Value1);
-            Console.WriteLine();
-            return Items[index];
-        }
-
-        Item Comparing(List<Item> Items, int index, int valueNumber)
-        {
-            bool matchSomeValue = false;　           //比較完了を表すbool
-            List<Item> ComparedItems;　　            //比較済みの要素
-            int comparingValue, comparedValue;     //比較する値の入れ物　値型じゃアカン気がする
-            string question;                                       //v1,v2で聞くことの切り替え
-
-            //V1,V2の切り替え
             if (valueNumber == 1)
             {
-                ComparedItems = V1ComparedItems;
-                comparingValue = Items[index].Value1;
-                question = " more necessary than ";
+                foreach (Item ComparedItem in ComparedItems)
+                {
+                    Console.WriteLine(result + ComparedItem.Name + " : " + ComparedItem.Value1);
+                }
+                Console.WriteLine(result + Items[index].Name + " : " + Items[index].Value1);
             }
             else
             {
-                ComparedItems = V2ComparedItems;
-                comparingValue = Items[index].Value2;
-                question = " more interesting than ";
-            }
-
-            //比較済みの各要素と比較
-            foreach (Item item in ComparedItems)
-            {
-                //V1,V2の切り替え
-                if (valueNumber == 1) comparedValue = item.Value1;
-                else comparedValue = item.Value2;
-
-                //比較する要素の値が比較先以下であり、かつ、どの要素の値ともイコールじゃない間ループする
-                //比較を進めて、全ての要素より大きくなるかどれかと価値が等しくなったらループを終了する
-                if (comparingValue <= comparedValue && matchSomeValue == false)
+                foreach (Item ComparedItem in ComparedItems)
                 {
-                    char choice;
-
-                    do
-                    {
-                        Console.Write("Is " + Items[index].Name + question + item.Name + "? : Equal:0 Yes:1 No:2  ==> ");
-                        do
-                        {
-                            choice = (char)Console.Read();
-                        } while (choice == '\n' || choice == '\r');
-                    } while (choice < '0' | choice > '2');
-
-                    switch (choice)
-                    {
-                        case '0':　//比較先の要素と価値がイコールである場合、boolをtrueにして比較を終了する
-                            Console.WriteLine(Items[index].Name + " == " + item.Name);
-                            comparingValue = comparedValue;
-                            matchSomeValue = true;
-                            break;
-                        case '1':　//比較先の要素より値が大きい場合、自信の値を比較先＋１して進める
-                            Console.WriteLine(Items[index].Name + " > " + item.Name);
-                            comparingValue = comparedValue + 1;
-                            Console.WriteLine("Debug : comparingValue is " + comparingValue);
-                            break;
-                        case '2':　//比較先の要素より値が小さい場合、比較済みの各要素の値を全て＋１して進める
-                            Console.WriteLine(Items[index].Name + " < " + item.Name);
-                            foreach (Item ComparedItem in ComparedItems)
-                            {
-                                if (valueNumber == 1 && ComparedItem.Value1 >= Items[index].Value1)
-                                {
-                                    ComparedItem.Value1++;
-                                }
-                                else if (valueNumber == 2 && ComparedItem.Value2 >= Items[index].Value2)
-                                {
-                                    ComparedItem.Value2++;
-                                }
-                            }
-                            break;
-                    }
+                    Console.WriteLine(result + ComparedItem.Name + " : " + ComparedItem.Value2);
                 }
+                Console.WriteLine(result + Items[index].Name + " : " + Items[index].Value2);
             }
-            if (valueNumber == 1) Items[index].Value1 = comparingValue;
-            else Items[index].Value2 = comparingValue;
 
-            //比較が終わったら値を表示して、返す
-            if (valueNumber == 1) Console.WriteLine("Necessity of " + Items[index].Name + " : " + Items[index].Value1);
-            else Console.WriteLine("Interests of " + Items[index].Name + " : " + Items[index].Value1);
             Console.WriteLine();
             return Items[index];
         }
